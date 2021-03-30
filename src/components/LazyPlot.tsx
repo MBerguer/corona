7b138f4147533bf06cc888ce4998e3bd71b82e02
Martin Berguer <martin.berguer@gmail.com>
2021-03-30 01:02:46 -0300

    config

--- src/components/LazyPlot.tsx
+++ src/components/LazyPlot.tsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import {Plotly} from './Plotly'
 
-export const LazyPlot = ({ layout, style, config, ...rest }) => {
+export const LazyPlot = ({ config, style, ...rest }) => {
   return (
     <Plotly
       {...rest}
       style={{ width: `100%`, ...style }}
+      config={{ locale: 'es' }}
     />
   )
 }
